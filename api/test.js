c31ad793bd5141a5465ce0e1edc1fa7728b7803b
Manas <imanaspaul@gmail.com>
2020-06-11 22:21:08 +0530

    Updated

--- api/test.js
+++ api/test.js
@@ -1,4 +1,13 @@
+let uu = require('url-unshort')();
+
 module.exports = (req, res) => {
-  const { name = 'World' } = req.query
-  res.status(200).send(`Hello ${name}!`)
+  const {url = 'http://goo.gl/HwUfwd' } = req.query
+
+  uu.expand('http://goo.gl/HwUfwd')
+  .then(url => {
+    if (url) res.status(200).send(`Original url is: ${url}`);
+    // no shortening service or an unknown one is used
+    else res.status(404).send(`No shorten url found`);
+  })
+  .catch(err => console.log(err));
 }
